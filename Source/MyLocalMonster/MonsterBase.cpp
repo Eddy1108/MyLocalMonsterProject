c6767e22b02b4efcf4ddbf4b633fb8a47ddec99f
Eddy1108 <edvardh@hotmail.no>
2022-03-10 20:03:07 +0100

    BEGIN

---
+++ Source/MyLocalMonster/MonsterBase.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonsterBase.h"
+
+// Sets default values
+AMonsterBase::AMonsterBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	StaticMesh->SetupAttachment(RootComponent);
+	//StaticMesh->SetRelativeRotationExact(FRotator(90.f, 0.f, 0.f));
+
+}
+
+// Called when the game starts or when spawned
+void AMonsterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMonsterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
